ca0f72f1c400220132e050ba23d2f606ded3ccb0
artegful <artegful@users.noreply.github.com>
2021-01-14 18:06:07 +0300

    Initial Commit

---
+++ Assets/Scripts/Gameplay/CollisionField.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class CollisionField : MonoBehaviour
+    {
+        private Collider collider;
+
+        private Action onFieldExit;
+
+        public void SetUpCallback(Collider collider, Action action)
+        {
+            this.collider = collider;
+            onFieldExit = action;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (onFieldExit != null)
+            {
+                if (other == collider)
+                {
+                    onFieldExit();
+                    onFieldExit = null;
+                    collider = null;
+                }
+
+            }
+        }
+    }
+}
+
